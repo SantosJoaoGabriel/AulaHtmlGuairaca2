3d6cc48cf69b276a980114d3c6da3df19e7a36a0
JoaoGabriel <joaopptopgg@gmail.com>
2024-10-01 21:33:43 -0300

    Update script.js
    4QuestaoAcertada

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 
 let numero1 = ("Por favor, informe o primeiro numero:");
-let numero2 = ("Por favor, informe um segundo número");
 
-let resultado = numero1 * numero2;
-
-console.log("O resultado da multiplicação é: ", resultado);
+for (let i = 1; <= 10; i++) {
+  console.log(`${numero} X ${i} = ${numero * i}`);
+}
 
 
