27433f7f98446a4b834ef745710cbd656f782769
Jaocomsono <joaopptopgg@gmail.com>
2024-10-07 21:22:12 -0300

    Feat:AulaJStarefa

--- script.js
+++ script.js
@@ -1,47 +1,20 @@
-// Função simples Declarativa)
-function saudacao() {
-    return console.log("Olá Mundo");
+function calculadora(a, b, operacao) {
+    if (operacao === 'soma') {
+        return a + b;
+    }
+    if (operacao === 'subtracao') {
+        return a - b;
+    }
+    if (operacao === 'multiplpicacao') {
+        return a * b;
+    }
+    if (operacao === 'divisao') {
+        return a / b;
+    }
+    return 'Operação invalida';
 }
 
-// Função com parametros
-function soam(valorA, valorB) {
-    const totalSoma = valorA + valorB;
-    return console.log(totalSoma);
-}
-
-// Função anônima
-const multiplicar = function (valorA, valorB) {
-    const total = valorA * valorB;
-    return console.log(total);
-}
-
-//  Arrow function
-const dividir = (a, b) => a / b;
-
-// Função com valor padrão de paramemtro
-function exponencial(base, expoente = 2) {
-    const exp = base ** expoente;
-    return console.log(exp);
-}
-
-// Invocar as funções
-saudacao();
-soam(10, 5);
-multiplicar(3, 6);
-exponencial(3);
-exponencial(3, 3);
-
-console.log(dividir(10, 2));
-
-// Função de callback
-function inicializaraplicacao() {
-    saudacao();
-soam(10, 5);
-multiplicar(3, 6);
-exponencial(3);
-exponencial(3, 3);
-
-console.log(dividir(10, 2));
-}
-
-inicializaraplicacao();
+console.log(calculadora(10, 5, 'soma'));
+console.log(calculadora(10, 5, 'subtracao'));
+console.log(calculadora(10, 5, 'multiplicacao'));
+console.log(calculadora(10, 5, 'divisao'))
